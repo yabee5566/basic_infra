f620ad29463ed77883408ccaede0a656fd9dcf74
Ian Pan <g-hamu@hotmail.com>
2021-09-13 00:39:27 +0800

    Initial commit

---
+++ app/src/main/java/com/ian_no_1/basic_infra/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ian_no_1.basic_infra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
