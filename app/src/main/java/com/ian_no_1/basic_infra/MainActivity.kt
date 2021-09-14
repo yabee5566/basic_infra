d91db660736b8c3a9b600f0a86725b8d68319260
Ian Pan <g-hamu@hotmail.com>
2021-09-14 19:54:08 +0800

    feat: add view binding, moshi, and retrofit dependency

--- app/src/main/java/com/ian_no_1/basic_infra/MainActivity.kt
+++ app/src/main/java/com/ian_no_1/basic_infra/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ian_no_1.basic_infra
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ian_no_1.basic_infra.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.tryMe.setOnClickListener{}
     }
+
+
 }
